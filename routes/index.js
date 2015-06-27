1c337931840d707a4865a00269c70287e8c3ef4a
muralimano28 <muralimano28@gmail.com>
2015-06-27 15:32:23 +0530

    Added code for search bar in routing file

--- routes/index.js
+++ routes/index.js
@@ -12,49 +12,26 @@ var options = {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-	/*var go = new Go(options, function(err){
-		if(err)
-		{
-			console.log("Error while connecting to Go");
-		}
-		go.execute({tag:'cute puppy'},function(result,response){
-			if(result.ok){
-				console.log('Go responded :' + response.data);			
-			}		
-		});
-		go.close();	
-	});*/
     res.sendFile(path.join(__dirname+"/../public/puppypull.html"));
 });
 
 router.get('/fetch',function(req,res,next){
-	/*var result ={
-    "photos": {
-        "page": 10,
-        "pages": "712",
-        "perpage": 10,
-        "total": "7114",
-        "photo": [
-            {
-                "id": "15534222683",
-                "owner": "17033374@N08",
-                "secret": "857719de68",
-                "server": "7580",
-                "farm": 8,
-                "title": "Life is better with a frenchie...",
-                "ispublic": 1,
-                "isfriend": 0,
-                "isfamily": 0
-            }
-        ]
-    },
-    "stat": "ok"
-} ;*/
 
 	/*************************************** Creating a http.get request to golang server**********************/
-	
-	var uri = "https://goflickr.herokuapp.com/fetch";
+	var tags = req.query.tag;
+    console.log("Received tag ",tags);
+    var uri;
+    if(tags == undefined)
+    {
+        
+        uri = "https://goflickr.herokuapp.com/fetch";
+        console.log(uri)
+    }
+    else
+    {
+        uri = "https://goflickr.herokuapp.com/fetch?tag="+tags;
+        console.log(uri)
+    }
 	request.get(uri,function(error,response,body){
 		res.send(body);	
 	});
@@ -62,9 +39,6 @@ router.get('/fetch',function(req,res,next){
 
 router.get('/upvote',function(req,res,next){
 	var id = req.query.id;
-	/*var parts = url.parse(req.url,true);
-	var query = parts.query;
-	var id = query.id;*/
 	console.log("Received parameter id is ",id);
 	var uri = "https://goflickr.herokuapp.com/upvote?id="+id;
 	request.get(uri,function(error,response,body){
@@ -79,9 +53,6 @@ router.get('/upvote',function(req,res,next){
 
 router.get('/downvote',function(req,res,next){
 	var id = req.query.id;
-	/*var parts = url.parse(req.url,true);
-	var query = parts.query;
-	var id = query.id;*/
 	console.log("Received parameter id is ",id);
 	var uri = "https://goflickr.herokuapp.com/downvote?id="+id;
 	request.get(uri,function(error,response,body){
